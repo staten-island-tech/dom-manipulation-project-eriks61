41c7c59a5143023888924b233492c137a4792bb7
eriks61 <eriks61@nycstudents.net>
2023-10-17 09:23:15 -0400

    43242

---
+++ index.js
@@ -0,0 +1,10 @@
+//const button = document.getElementById("btn");
+//console.log(button)
+//const text = document.querySelector("#text");
+//"text" can change depending on index.html
+const DOMSelectors = {
+    button: document.getElementById("btn"),
+    //text:document.querySelector("#text"),
+    point: document.querySelector(".point"),
+};
+console.log(DOMSelectors.point);
