5ab207950b5de80b314e9e17cf57f404fa29c0ba
eriks61 <eriks61@nycstudents.net>
2023-10-19 09:21:44 -0400

    324234

--- index.js
+++ index.js
@@ -4,7 +4,23 @@
 //"text" can change depending on index.html
 const DOMSelectors = {
     button: document.getElementById("btn"),
-    //text:document.querySelector("#text"),
-    point: document.querySelector(".point"),
+    text:document.querySelector("#text"),
+    point: document.querySelectorAll(".point"),
+    box: document.getElementById("big-black-box"),
 };
-console.log(DOMSelectors.point);
+function backgroundAndText(background, text){
+    background.style.backgroundColor = "red";
+    text.textContent = "This is now a big red box";
+    text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function(){
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi(){
+    DOMSelectors.points.forEach((point)=>{
+        console.log(point);
+    });
+}
+changeLi();
