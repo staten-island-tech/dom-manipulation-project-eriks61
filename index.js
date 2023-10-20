b1398f9d960d0cfad6a5db99d50c9b5edc9d8117
eriks61 <eriks61@nycstudents.net>
2023-10-20 09:21:45 -0400

    345

--- index.js
+++ index.js
@@ -2,25 +2,40 @@
 //console.log(button)
 //const text = document.querySelector("#text");
 //"text" can change depending on index.html
+
+//function backgroundAndText(background, text){
+ //   background.style.backgroundColor = "red";
+   // text.textContent = "This is now a big red box";
+    //text.style.fontSize = "40px";
+//}
+
+//DOMSelectors.button.addEventListener("click", function(){
+//backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+//});
+
+//function changeLi(){
+ //   DOMSelectors.point.forEach((point)=>{
+ //      point.addEventListener("click", function () {
+
+ //      })
+  //  });
+//}
+//changeLi();
+
+//function makeAlbum(){
+ //   //const album = somethign{}
+    //return an object
+//}
+
+//function addCard(Album){
+
+//}
+
 const DOMSelectors = {
     button: document.getElementById("btn"),
-    text:document.querySelector("#text"),
-    point: document.querySelectorAll(".point"),
+    input: document.getElementById(`#input`),
     box: document.getElementById("big-black-box"),
 };
-function backgroundAndText(background, text){
-    background.style.backgroundColor = "red";
-    text.textContent = "This is now a big red box";
-    text.style.fontSize = "40px";
-}
 
-DOMSelectors.button.addEventListener("click", function(){
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-
-function changeLi(){
-    DOMSelectors.points.forEach((point)=>{
-        console.log(point);
-    });
-}
-changeLi();
+DOMSelectors.box.insertAdjacentElement("beforebegin", `<h1>We are an H1</h1>` 
+)
