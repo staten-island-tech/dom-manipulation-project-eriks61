53046b8a9e17030baa1818fd8035622830b14676
eriks61 <eriks61@nycstudents.net>
2023-10-27 09:19:28 -0400

    fytr7

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 //"text" can change depending on index.html
 
 //function backgroundAndText(background, text){
- //   background.style.backgroundColor = "red";
-   // text.textContent = "This is now a big red box";
-    //text.style.fontSize = "40px";
+//   background.style.backgroundColor = "red";
+// text.textContent = "This is now a big red box";
+//text.style.fontSize = "40px";
 //}
 
 //DOMSelectors.button.addEventListener("click", function(){
@@ -14,17 +14,17 @@
 //});
 
 //function changeLi(){
- //   DOMSelectors.point.forEach((point)=>{
- //      point.addEventListener("click", function () {
+//   DOMSelectors.point.forEach((point)=>{
+//      point.addEventListener("click", function () {
 
- //      })
-  //  });
+//      })
+//  });
 //}
 //changeLi();
 
 //function makeAlbum(){
- //   //const album = somethign{}
-    //return an object
+//   //const album = somethign{}
+//return an object
 //}
 
 //function addCard(Album){
@@ -32,28 +32,39 @@
 //}
 
 const DOMSelectors = {
-    button: document.getElementById("btn"),
-    card: document.querySelector(".card"),
-    input1: document.querySelector("input[name='FullName']"),
-    input2: document.querySelector("input[name='Age']"),
-    input3: document.querySelector("input[name='Occupation']"),
-  };
+  button: document.getElementById("btn"),
+  card: document.querySelector(".card"),
+  input1: document.querySelector("input[name='FullName']"),
+  input2: document.querySelector("input[name='Image']"),
+  input3: document.querySelector("input[name='Occupation']"),
+};
 
 /* const cat = "meow";
 DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat}</h1>` )
  */
-DOMSelectors.button.addEventListener("click", function(){
-let input1Value=DOMSelectors.input1.value;
-let input2Value=DOMSelectors.input2.value;
-let input3Value=DOMSelectors.input3.value;
+DOMSelectors.button.addEventListener("click", function () {
+  let input1Value = DOMSelectors.input1.value;
+  let input2Value = DOMSelectors.input2.value;
+  let input3Value = DOMSelectors.input3.value;
 
-if(input1Value && input2Value && input3Value){
-  const cardInfo = document.createElement("div");
-  cardInfo.insertAdjacentHTML("afterbegin", `<p>${input1Value}</p><p>${input2Value}</p><p>${input3Value}</p>`)
-  DOMSelectors.card.appendChild(cardInfo);
+  if (input1Value && input2Value && input3Value) {
+    const cardInfo = document.createElement("div");
+    cardInfo.insertAdjacentHTML(
+      "afterbegin",
+      `<p>${input1Value}</p><p>${input2Value}</p><p>${input3Value}</p>`
+    );
+    DOMSelectors.card.appendChild(cardInfo);
 
-  DOMSelectors.input1.value = "";
-  DOMSelectors.input2.value = "";
-  DOMSelectors.input3.value = "";
-}
+    DOMSelectors.input1.value = "";
+    DOMSelectors.input2.value = "";
+    DOMSelectors.input3.value = "";
+  }
 });
+
+DOMSelectors.button.addEventListener(
+  "click",
+  () => {
+    DOMSelectors.getElementById("card").visibility = visible;
+  },
+  false
+);
